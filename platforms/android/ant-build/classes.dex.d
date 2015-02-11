05e14c7258ba1d6f45eef7a47034c03474ea2aa5
ubuntu <oalbaab14dw@ikzubirimanteo.com>
2015-02-11 09:50:25 +0100

    fondo txuria jarrita

--- platforms/android/ant-build/classes.dex.d
+++ platforms/android/ant-build/classes.dex.d
@@ -6,4 +6,11 @@
 /home/ubuntu/ogames/platforms/android/ant-build/classes/com/phonegap/helloworld/CordovaApp.class \
 /home/ubuntu/ogames/platforms/android/ant-build/classes/com/phonegap/helloworld/R$string.class \
 /home/ubuntu/ogames/platforms/android/ant-build/classes/com/phonegap/helloworld/R$attr.class \
+/home/ubuntu/ogames/platforms/android/ant-build/classes/com/odeialba/ogames/BuildConfig.class \
+/home/ubuntu/ogames/platforms/android/ant-build/classes/com/odeialba/ogames/R.class \
+/home/ubuntu/ogames/platforms/android/ant-build/classes/com/odeialba/ogames/R$xml.class \
+/home/ubuntu/ogames/platforms/android/ant-build/classes/com/odeialba/ogames/R$drawable.class \
+/home/ubuntu/ogames/platforms/android/ant-build/classes/com/odeialba/ogames/CordovaApp.class \
+/home/ubuntu/ogames/platforms/android/ant-build/classes/com/odeialba/ogames/R$string.class \
+/home/ubuntu/ogames/platforms/android/ant-build/classes/com/odeialba/ogames/R$attr.class \
 /home/ubuntu/ogames/platforms/android/ant-build/dexedLibs/classes-4370628ea59626b3860ceaa8bbd85ed7.jar \
